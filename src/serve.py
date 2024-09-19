c18e3fc7ee8b19387913e93cff89296dafc72d1d
Nathan Souza <naathanmarinho11@gmail.com>
2024-09-19 20:04:49 -0300

    rodando

--- src/serve.py
+++ src/serve.py
@@ -8,5 +8,9 @@ svc = bentoml.Service("iris_classifier", runners=[iris_clf_runner])
 
 @svc.api(input=NumpyNdarray(), output=NumpyNdarray())
 def classify(input_series: np.ndarray) -> np.ndarray:
+    # Check if input is 1D and reshape if necessary
+    if input_series.ndim == 1:
+        input_series = input_series.reshape(1, -1)
+    
     result = iris_clf_runner.predict.run(input_series)
     return result
